a047a8c9806976199ec5f7509a67208b0e66180d
Timo Sandmann <mail@timosandmann.de>
2016-03-05 15:39:37 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# armv8l-toolchain-linux
+Toolchain for linux x86_64 host and armv8l-linux-gnueabihf target optimized for cortex-a53 (Raspberry Pi 3).
+
+Checkout to /usr/local/armv8l-unknown-linux-gnueabihf and add /usr/local/armv8l-unknown-linux-gnueabihf/bin to PATH
---
+++ lib/gcc/armv8l-unknown-linux-gnueabihf/5.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/tmp/ct-ng-build/src/gcc-5.2.0/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=armv8l-unknown-linux-gnueabihf --prefix=/usr/local/armv8l-unknown-linux-gnueabihf --with-sysroot=/usr/local/armv8l-unknown-linux-gnueabihf/armv8l-unknown-linux-gnueabihf/sysroot --enable-languages=c,c++ --with-cpu=cortex-a53 --with-fpu=neon-fp-armv8 --with-float=hard --with-pkgversion='crosstool-NG crosstool-ng-1.22.0' --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --with-gmp=/tmp/ct-ng-build/armv8l-unknown-linux-gnueabihf/buildtools --with-mpfr=/tmp/ct-ng-build/armv8l-unknown-linux-gnueabihf/buildtools --with-mpc=/tmp/ct-ng-build/armv8l-unknown-linux-gnueabihf/buildtools --with-isl=/tmp/ct-ng-build/armv8l-unknown-linux-gnueabihf/buildtools --with-cloog=/tmp/ct-ng-build/armv8l-unknown-linux-gnueabihf/buildtools --with-libelf=/tmp/ct-ng-build/armv8l-unknown-linux-gnueabihf/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --disable-nls --disable-multilib --with-local-prefix=/usr/local/armv8l-unknown-linux-gnueabihf/armv8l-unknown-linux-gnueabihf/sysroot --enable-long-long --with-march=armv8-a+crc";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "cortex-a53" }, { "float", "hard" }, { "fpu", "neon-fp-armv8" }, { "tls", "gnu" } };
